63deb3230fd0172e86373be5553eca54a4221150
László Tuss <2952613+laszlotuss@users.noreply.github.com>
2024-01-01 19:13:51 +0100

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 import PackageDescription
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let dependencies: [Package.Dependency] = []
-#else
-let dependencies: [Package.Dependency] = [.package(url: "https://github.com/Meniny/Zipper.git", .exact("1.0.2"))]
-#endif
 
 let package = Package(
     name: "Zipper",
     products: [
-        .library(name: "Zipper", targets: ["Zipper"])
+        .library(
+            name: "Zipper",
+            targets: ["Zipper"]
+        )
     ],
-	dependencies: dependencies,
+    dependencies: [ ],
     targets: [
-        .target(name: "Zipper"),
-		.testTarget(name: "ZipperTests", dependencies: ["Zipper"])
+        .target(
+            name: "Zipper",
+            dependencies: [],
+            path: "Zipper/Source"
+        )
     ]
 )
